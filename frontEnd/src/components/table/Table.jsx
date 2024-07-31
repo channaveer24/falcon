83718becbc01104d46f87c71f237667dd050a3db
Channaveer Patil <cjchannaveer2423@gmail.com>
2024-08-01 00:04:54 +0530

    api change

--- frontEnd/src/components/table/Table.jsx
+++ frontEnd/src/components/table/Table.jsx
@@ -447,7 +447,7 @@ function App() {
     const fetchData = async () => {
       try {
         const result = await axios.get(
-          "https://falcon-reality-server.vercel.app/api/getChatdata"
+          "https://falconreality.onrender.com/api/getChatdata"
         );
         // const result = await axios.get(
         //   "http://localhost:8800/api/getChatData"
